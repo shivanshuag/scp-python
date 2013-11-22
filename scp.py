c81b4b52ce66817d34d847fc301dbc7ddce4aab6
shivanshuag <shivanshuag@gmail.com>
2013-11-23 02:18:57 +0530

    implementing r2r

--- scp.py
+++ scp.py
@@ -1,14 +1,19 @@
+import threading
+import Queue
 import getopt
 import sys
 import os
 import paramiko
 import getpass
+from socket import timeout as SocketTimeout
 
-
+global buff_size
 buff_size = 16384
 global recursive
 recursive = False
-
+global channelTo, channelFrom
+msg = None
+ack = None
 def ack(channel):
     # read scp response
     recv = ''
@@ -43,32 +48,6 @@ def read_stats(name):
 	mtime = int(stats.st_mtime)
 	return (mode, size, mtime, atime)
 
-def send_files(file):
-	basename = os.path.basename(file)
-	(mode, size, mtime, atime) = read_stats(file)
-	# if preserve_times:
-	#     _send_time(mtime, atime)
-	file_hdl = open(file, 'rb')
-
-	# The protocol can't handle \n in the filename.
-	# Quote them as the control sequence \^J for now,
-	# which is how openssh handles it.
-	channel.sendall("C%s %d %s\n" %
-	                     (mode, size, basename.replace('\n', '\\^J')))
-	recv_confirm()
-	file_pos = 0
-	# if _progress:
-	#     _progress(basename, size, 0)
-	buff_size = 16384
-	chan = channel
-	while file_pos < size:
-	    chan.sendall(file_hdl.read(buff_size))
-	    file_pos = file_hdl.tell()
-	    # if _progress:
-	    #     _progress(basename, size, file_pos)
-	chan.sendall('\x00')
-	file_hdl.close()
-	recv_confirm()
 
 
 def upload_nextDir(channel,directory):
@@ -110,7 +89,7 @@ def createSSHChannel(username, host):
 	pbulicKeyFile = os.path.expanduser("~")+'/.ssh/id_rsa.pub'
 	try:
 		ssh.connect(host, username=username, key_filename=pbulicKeyFile,  look_for_keys=True)
-	except paramiko.AuthenticationException:
+	except:
 		print 'public key authentication failed, enter password for '+username+'@'+host
 		password = getpass.getpass('Password:')
 		try:
@@ -124,6 +103,7 @@ def createSSHChannel(username, host):
 
 
 def upload_single_file(channel, dirFrom):
+	global buff_size
 	filename = os.path.basename(dirFrom)
 	fil = open(dirFrom,'rb')
 	(mode, size, mtime, atime) = read_stats(dirFrom)
@@ -169,6 +149,290 @@ def upload(usernameTo, hostTo, dirTo, dirFrom):
 		channel.exec_command(scp_command)
 		ack(channel)
 		upload_single_file(channel, dirFrom)
+
+
+def download(usernameFrom, hostFrom, dirFrom, dirTo):
+	print "downloadinf file from host"
+	ssh = createSSHChannel(usernameFrom, hostFrom)
+	transport = ssh.get_transport()
+	channel = transport.open_session()
+	channel.settimeout(100000.0)
+	dirFrom = "'" + dirFrom.replace("'", "'\"'\"'") + "'"
+	if recursive:
+		scp_command = "scp -r -p -f "+dirFrom
+		channel.exec_command(scp_command)
+	else:
+		scp_command = "scp -p -f "+dirFrom
+		channel.exec_command(scp_command)
+
+
+	while not channel.closed:
+		#print "inside while"
+		global buff_size
+		channel.sendall('\x00')
+		msg = channel.recv(1024)
+		if not msg: # channel closed while receiving
+			print "Success"
+			sys.exit(0)
+		if(msg[0]=='C'):   #recv_file
+			parts = msg[1:].strip().split(' ', 2)
+			mode = int(parts[0], 8)
+			size = int(parts[1])
+			path = os.path.join(dirTo, parts[2])
+			fil = file(path, 'wb')
+			pos = 0
+			channel.send('\x00')
+			try:
+				while pos < size:
+					if size - pos <= buff_size:
+						buff_size = size - pos
+						fil.write(channel.recv(buff_size))
+					pos = fil.tell()
+					fil.truncate()
+				msg = channel.recv(512)
+				if msg and msg[0] != '\x00':
+					print "exception\n "+msg[1:]
+					sys.exit(0)
+			except SocketTimeout:
+				channel.close()
+				print "exception Socket timeout"
+				sys.exit(0)
+			fil.truncate()
+			os.utime(path, utime)
+			utime = None
+			os.chmod(path, mode)
+
+			l=len(path)
+			print "filename =",str(path)
+			for i in range(l,20):  print " ",
+			print "size     = "+str(size)+" bytes"
+			fil.close()
+
+		elif(msg[0]=='D'):   #next dir
+			parts = msg[1:].split()
+			try:
+				mode = int(parts[0], 8)
+				path = os.path.join(dirTo, parts[2])
+			except:
+				channel.send('\x01')
+				print 'Bad directory format'
+				sys.exit(0)
+			try:
+				if not os.path.exists(path):
+					print 'no directory exists'
+					os.mkdir(path, mode)
+				elif os.path.isdir(path):
+					os.chmod(path, mode)
+				else:
+					print path + 'is not a directory'
+			except:
+				channel.send('\x01no directory')
+				print 'no such directory'
+				sys.exit(0)
+			dirTo = path
+		elif(msg[0]=='E'):		#go to prev directory
+			dirTo = os.path.split(dirTo)[0]
+		elif(msg[0]=='T'):
+			times = msg[1:].split()
+			mtime = int(times[0])
+			atime = int(times[2]) or mtime
+			utime = (atime, mtime)
+
+def handle_sshTo(usernameTo, hostTo, dirTo):
+	global channelFrom
+	sshTo = createSSHChannel(usernameTo, hostTo)
+	transportTo = sshTo.get_transport()
+	channelTo = transportTo.open_session()
+	channelTo.settimeout(5.0)
+	dirTo = "'" + dirTo.replace("'", "'\"'\"'") + "'"
+	cd_cmd = "cd "+dirTo
+	#channelTo.exec_command(cd_cmd)
+
+	if recursive:
+		scp_command = cd_cmd+"&& scp -r -t "+dirTo
+		channelTo.exec_command(scp_command)
+	else:
+		scp_command = cd_cmd+"&& scp -t "+dirTo
+		channelTo.exec_command(scp_command)
+
+
+	while not channelFrom.closed:
+		ack = channelTo.recv(512)
+		while msg == None:
+			print "jkdshf"
+			pass
+		channelTo.sendall(msg)
+		if(msg[0] == 'C'):
+			msg = None
+			ack = channelTo.recv(512)
+			pos = 0
+			parts = msg[1:].strip().split(' ', 2)
+			size = int(parts[1])
+			while pos < size:
+				if size - pos <= buff_size:
+					buff_size = size - pos
+				while msg==None:
+					pass
+				channelTo.sendall(msg)
+				pos += len(msg)
+				msg = None
+			while ack == None:
+				pass
+			channelTo.sendall(ack)
+			ack = None
+
+
+
+
+def handle_sshFrom(usernameFrom, hostFrom, dirFrom):
+	sshFrom = createSSHChannel(usernameFrom, hostFrom)
+	transportFrom = sshFrom.get_transport()
+	channelFrom = transportFrom.open_session()
+	channelFrom.settimeout(5.0)
+	dirFrom = "'" + dirFrom.replace("'", "'\"'\"'") + "'"
+	if recursive:
+		scp_command = "scp -r -p -f "+dirFrom
+		channelFrom.exec_command(scp_command)
+	else:
+		scp_command = "scp -p -f "+dirFrom
+		channelFrom.exec_command(scp_command)
+
+		while not channelFrom.closed:
+		#print "inside while"
+			while(ack == None):
+				pass
+			channelFrom.sendall(ack)
+			ack = None
+			msg = channelFrom.recv(1024)
+			#channelTo.sendall(msg)
+		if not msg: # channelFrom closed while receiving
+			print "Success"
+			sys.exit(0)
+		if(msg[0]=='C'):   #recv_file
+			parts = msg[1:].strip().split(' ', 2)
+			# mode = int(parts[0], 8)
+			size = int(parts[1])
+			# path = os.path.join(dirTo, parts[2])
+			# fil = file(path, 'wb')
+			# pos = 0
+			while ack == None:
+				pass
+			channelFrom.send(ack)
+			ack = None
+			try:
+				while pos < size:
+					if size - pos <= buff_size:
+						buff_size = size - pos
+						msg = channelFrom.recv(buff_size)
+						#channelTo.sendall(content)
+					pos += len(content)
+					#fil.truncate()
+				ack = channelFrom.recv(512)
+				# if msg and msg[0] != '\x00':
+				# 	print "exception\n "+msg[1:]
+				# 	sys.exit(0)
+			except SocketTimeout:
+				channelFrom.close()
+				print "exception Socket timeout"
+				sys.exit(0)
+
+
+def remoteToremote(usernameTo, hostTo, dirTo, usernameFrom, hostFrom, dirFrom):
+	global cha
+	print "inside r2r"	
+	t1 = threading.Thread(target=handle_sshTo, args=(usernameTo, hostTo, dirTo))
+	t1.daemon = True
+	t1.start()
+	t2 = threading.Thread(target=handle_sshFrom, args=(usernameFrom, hostFrom, dirFrom))
+	t2.daemon = True
+	t2.start()
+	t2.join()
+	t1.join()
+	#dirFrom = "'" + dirFrom.replace("'", "'\"'\"'") + "'"
+	# if recursive:
+	# 	scp_command = "scp -r -p -f "+dirFrom
+	# 	channelFrom.exec_command(scp_command)
+	# else:
+	# 	scp_command = "scp -p -f "+dirFrom
+	# 	channelFrom.exec_command(scp_command)
+	# cd_cmd = "cd "+dirTo
+	# channelTo.exec_command(cd_cmd)
+	# channelTo.exec_command('pwd')
+	# while (not channelTo.recv(buff_size)):
+	# 	pass
+	
+	# while not channelFrom.closed:
+	# 	#print "inside while"
+	# 	channelFrom.sendall('\x00')
+	# 	msg = channelFrom.recv(1024)
+	# 	channelTo.sendall(msg)
+	# 	if not msg: # channelFrom closed while receiving
+	# 		print "Success"
+	# 		sys.exit(0)
+	# 	if(msg[0]=='C'):   #recv_file
+	# 		# parts = msg[1:].strip().split(' ', 2)
+	# 		# mode = int(parts[0], 8)
+	# 		# size = int(parts[1])
+	# 		# path = os.path.join(dirTo, parts[2])
+	# 		# fil = file(path, 'wb')
+	# 		# pos = 0
+	# 		channelFrom.send(channelTo.recv(512))
+	# 		try:
+	# 			while pos < size:
+	# 				if size - pos <= buff_size:
+	# 					buff_size = size - pos
+	# 					content = channelFrom.recv(buff_size)
+	# 					channelTo.sendall(content)
+	# 				pos += len(content)
+	# 				#fil.truncate()
+	# 			channelTo.sendall(channelFrom.recv(512))
+	# 			# if msg and msg[0] != '\x00':
+	# 			# 	print "exception\n "+msg[1:]
+	# 			# 	sys.exit(0)
+	# 		except SocketTimeout:
+	# 			channelFrom.close()
+	# 			print "exception Socket timeout"
+	# 			sys.exit(0)
+			# fil.truncate()
+			# os.utime(path, utime)
+			# utime = None
+			# os.chmod(path, mode)
+
+			# l=len(path)
+			# print "filename =",str(path)
+			# for i in range(l,20):  print " ",
+			# print "size     = "+str(size)+" bytes"
+			# fil.close()
+
+		# elif(msg[0]=='D'):   #next dir
+		# 	parts = msg[1:].split()
+		# 	try:
+		# 		mode = int(parts[0], 8)
+		# 		path = os.path.join(dirTo, parts[2])
+		# 	except:
+		# 		channelFrom.send('\x01')
+		# 		print 'Bad directory format'
+		# 		sys.exit(0)
+		# 	try:
+		# 		if not os.path.exists(path):
+		# 			print 'no directory exists'
+		# 			os.mkdir(path, mode)
+		# 		elif os.path.isdir(path):
+		# 			os.chmod(path, mode)
+		# 		else:
+		# 			print path + 'is not a directory'
+		# 	except:
+		# 		channelFrom.send('\x01no directory')
+		# 		print 'no such directory'
+		# 		sys.exit(0)
+		# 	dirTo = path
+		# elif(msg[0]=='E'):		#go to prev directory
+		# 	dirTo = os.path.split(dirTo)[0]
+		# elif(msg[0]=='T'):
+		# 	times = msg[1:].split()
+		# 	mtime = int(times[0])
+		# 	atime = int(times[2]) or mtime
+		# 	utime = (atime, mtime)
 
 
 def main(argv):
@@ -238,36 +502,7 @@ def main(argv):
 	else :
 		remoteToremote(usernameTo, hostTo, dirTo, usernameFrom, hostFrom, dirFrom)
 
-	# try:
-	# 	ssh.connect("webhome.cc.iitk.ac.in", username='agrawals',key_filename=pbulicKeyFile,  look_for_keys=True)
-	# except paramiko.AuthenticationException:
-	# 	print 'public key authentication failed, enter password'
-	# 	pswd = getpass.getpass('Password:')
-	# 	try:
-	# 		ssh.connect("webhome.cc.iitk.ac.in", username='agrawals', password=pswd)
-	# 	except paramiko.AuthenticationException:
-	# 		print"Authentication failed"
-	# 		return
-
-	# transport = ssh.get_transport()
-	# channel = transport.open_session()
-	# channel.settimeout(5.0)
-
-
-	# channel.exec_command("pwd")
-	# msg = channel.recv(512)
-
-
-	# channel.exec_command("scp -t ~/")
-	# msg = channel.recv(512)
-	# print msa
-	# send_files('sent.txt')
 
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
-
-#print "sfadfsa"
-# stdin, stdout, stderr = ssh.exec_command("uptime")
-# type(stdin)
-# stdout.readlines()
